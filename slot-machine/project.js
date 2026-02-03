f316b02c9e61c1e0980ade813a81914aed55c780
pixelbushi <opiyosbenji@gmail.com>
2026-02-03 11:54:03 +0300

    Fix: re-add slot-machine as regular directory

---
+++ slot-machine/project.js
@@ -0,0 +1,15 @@
+// Deposit some money
+// Determin number of lines to bet on
+// Collect a bet amount
+// Spin the slot machine
+// Check if the user won
+// Give the user their winnings
+// Play again or quit
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+   const depositAmount = prompt("Enter deposit amount: ");
+}
+ 
+deposit();
